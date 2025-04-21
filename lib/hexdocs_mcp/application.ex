5a1d1c64e62d13140e46fb56a22d2086ca8ff63b
Bradley Golden <golden.bradley@gmail.com>
2025-04-21 09:08:50 -0500

    fix sqlite vec loading to incorrect path

--- lib/hexdocs_mcp/application.ex
+++ lib/hexdocs_mcp/application.ex
@@ -4,38 +4,35 @@ defmodule HexdocsMcp.Application do
 
   @impl true
   def start(_type, _args) do
-    download_sqlite_vec()
+    ensure_sqlite_vec_available()
 
     children = [
-      {HexdocsMcp.Repo, load_extensions: [SqliteVec.path()], database: HexdocsMcp.Config.database()}
+      {HexdocsMcp.Repo, load_extensions: [sqlite_vec_extension_path()], database: HexdocsMcp.Config.database()}
     ]
 
     opts = [strategy: :one_for_one, name: HexdocsMcp.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  defp download_sqlite_vec do
+  defp ensure_sqlite_vec_available do
     version = SqliteVec.Downloader.default_version()
-
-    output_dir =
-      :hexdocs_mcp
-      |> :code.priv_dir()
-      |> Path.join(version)
-      |> tap(&File.mkdir_p!/1)
+    output_dir = sqlite_vec_dir(version)
+    File.mkdir_p!(output_dir)
 
     case SqliteVec.download(output_dir, version) do
-      :skip ->
-        :ok
-
-      {:ok, _successful_files, []} ->
-        :ok
-
-      {:ok, _successful_files, failed_files} ->
-        message = "failed to download: " <> Enum.join(failed_files, ", ")
-        raise(message)
-
-      {:error, message} ->
-        raise(message)
+      :skip -> :ok
+      {:ok, _, []} -> :ok
+      {:ok, _, failed_files} -> raise "Failed to download: #{Enum.join(failed_files, ", ")}"
+      {:error, message} -> raise message
     end
+  end
+
+  defp sqlite_vec_extension_path do
+    version = SqliteVec.Downloader.default_version()
+    Path.join(sqlite_vec_dir(version), "vec0")
+  end
+
+  defp sqlite_vec_dir(version) do
+    Path.join([HexdocsMcp.Config.data_path(), "sqlite_vec", version])
   end
 end
